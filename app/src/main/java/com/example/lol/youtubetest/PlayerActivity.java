36ddbd71931590ebc6216b79efd982d4bb9459a8
kareemelbayaa <kareemelbayaa@gmail.com>
2015-05-02 20:38:23 +0200

    Summary
    Description

---
+++ app/src/main/java/com/example/lol/youtubetest/PlayerActivity.java
@@ -0,0 +1,40 @@
+package com.example.lol.youtubetest;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+/**
+ * Created by LOL on 01/05/2015.
+ */
+public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private YouTubePlayerView playerView;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.activity_player);
+
+        playerView = (YouTubePlayerView)findViewById(R.id.player_view);
+        playerView.initialize(YoutubeConnector.KEY, this);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult result) {
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean restored) {
+        if(!restored){
+            player.cueVideo(getIntent().getStringExtra("VIDEO_ID"));
+        }
+    }
+}
